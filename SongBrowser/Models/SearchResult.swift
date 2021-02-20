3ab6728896c4f6278a2285d48cc5681e860b4db5
Bradley Kang <bkang@lcs.on.ca>
2021-02-20 10:15:40 -0500

    Defined the model for the parts of the JSON file that we care about retrieving

---
+++ SongBrowser/Models/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  SongBrowser
+//
+//  Created by Brad Kang on 2021-02-20.
+//
+
+import Foundation
+
+struct SearchResult:Decodable {
+    
+    let results: [Song]
+}
