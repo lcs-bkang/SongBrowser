985f058d9edc73d3c8101b1cda0026ef7d066266
Bradley Kang <bkang@lcs.on.ca>
2021-02-22 11:56:21 -0500

    Added views to add a navigation view when the songs are clicked on, and allow them to be played

--- SongBrowser/SongBrowserApp.swift
+++ SongBrowser/SongBrowserApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SongBrowserApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
